146f0939b03a03fa81718631365ccd4a4ca5e8fd
Vaibhav Yadav <tron21vaibhav@gmail.com>
2023-08-04 00:26:28 +0530

    Update Environment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './App.css';
 import TaskList from './Components/TaskList';
 import "./Components/UpdateTask/UpdateTask.css"
+import InsecureContent from "./InsecureContent/InsecureContent"
 function App() {
 
 
   return (
     <div className='Task'>
-
       <div className='createtaskcontainer'>
         <p className='Headings'>My Task Manager</p>
         <Link to="/createtask"><div className='CreateTaskbtn'><p className='CreateTasktxt'>+ Create New Task</p></div></Link>
--- src/Router.jsx
+++ src/Router.jsx
@@ -4,6 +4,7 @@ import CreateTask from './Components/CreateTask';
 import ErrorElement from './Components/ErrorElements';
 import NotFound from './Components/NotFound';
 import UpdateTask from './Components/UpdateTask/UpdateTask';
+import InsecureContent from './InsecureContent/InsecureContent';
 
 
 export default function Routing() {
@@ -11,6 +12,7 @@ export default function Routing() {
     let route = createBrowserRouter(createRoutesFromElements(
         <Route element={<Outlet />} errorElement={<ErrorElement/>} >
             <Route index element={<App />} />
+            <Route path='/note' element={<InsecureContent />} />
             <Route path='/createtask' element={<CreateTask />} />
             <Route path='/taskupdate' element={<UpdateTask />} />
             <Route path="*" element={<NotFound />} />
